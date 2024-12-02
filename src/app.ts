c0e6a58117ccb66b275df9f12e969aa4ad2de85c
MrGalva07 <lucas.galvao0707@gmail.com>
2024-12-02 10:30:34 -0300

    rendering-product-in-cart

--- src/app.ts
+++ src/app.ts
@@ -16,17 +16,3 @@ for (const product of data) {
   ).toHTML();
 }
 
-/*
-const product1 = new Product("Banana", "Fruta", 10, "http://example.com");
-product1.incrementQuantity();
-product1.incrementQuantity();
-product1.incrementQuantity();
-
-const product2 = new Product("Maçã", "Fruta", 5, "http://example.com");
-product2.incrementQuantity();
-product2.incrementQuantity();
-console.log(Cart);
-
-Cart.removeProduct(product2);
-console.log(Cart);
-*/
