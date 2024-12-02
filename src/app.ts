a09875adee33e201d436488081387b17cf5808b7
MrGalva07 <lucas.galvao0707@gmail.com>
2024-12-02 10:28:39 -0300

    randomRender

---
+++ src/app.ts
@@ -0,0 +1,32 @@
+// importar data.json
+// importar entities/product.ts
+// Criar os objetos de produto utilizando os
+// dados de data.json
+
+import { Product } from "./entities/product";
+import data from "../data.json";
+import { Cart } from "./entities/cart";
+
+for (const product of data) {
+  new Product(
+    product.name,
+    product.category,
+    product.price,
+    product.image.desktop
+  ).toHTML();
+}
+
+/*
+const product1 = new Product("Banana", "Fruta", 10, "http://example.com");
+product1.incrementQuantity();
+product1.incrementQuantity();
+product1.incrementQuantity();
+
+const product2 = new Product("Maçã", "Fruta", 5, "http://example.com");
+product2.incrementQuantity();
+product2.incrementQuantity();
+console.log(Cart);
+
+Cart.removeProduct(product2);
+console.log(Cart);
+*/
